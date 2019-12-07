a30a8cab16020bf2dd01c84a13506c8a1d571cd0
Hacker-kingdom-TH673 <58379849+Hacker-kingdom-TH673@users.noreply.github.com>
2019-12-07 17:12:25 +0700

    LOAD-command

---
+++ number.py
@@ -0,0 +1,17 @@
+import os,sys
+os.system("figlet Hacker Kingdom th")
+A = input('Number : ')
+B = input('Number : ')
+
+print
+print "\033[1m\033[32m programming By Hacker Kingdom TH"
+print
+print "\033[0m"
+os.system("clear")
+print "\033[1m\033[32m programming By Hacker Kingdom TH"
+print
+print
+print
+print
+print
+print (A + B)
